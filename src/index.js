9321f40d9201d0393a75fb6f73ce313028f9b7f9
imadhahmed <imadhahmeth08@gmail.com>
2025-08-17 02:00:33 +0530

    undo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";  // Import BrowserRouter
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/portfolio">  {/* Add basename for GitHub Pages */}
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
