2b16db788e439e902c79a5722114a99e4d8311fd
imadhahmed <imadhahmeth08@gmail.com>
2025-08-17 01:52:11 +0530

    jju

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-router-dom";
+import ReactDOM from "react-dom";  // Correct import
+import { BrowserRouter as Router } from "react-router-dom";  // Correct Router import
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
